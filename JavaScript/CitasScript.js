4f89cb23605691b7462ac7ab95f56ce5cab72f8f
allan-pardo <20250394@ricaldone.edu.sv>
2026-06-21 22:15:30 -0600

    Mi parte

---
+++ JavaScript/CitasScript.js
@@ -0,0 +1,50 @@
+/*
+
+Chicos esto lo dejare asi porque me falta terminarlo bien
+
+*/
+
+
+
+
+/*const mensajeVacio = document.getElementById("mensajeVacio");
+const listaCitas = document.getElementById("listaCitas");
+
+let citas = [
+  {
+    fechaHora: "14/5/2026, 11:25 A.M",
+    estudiante: "David Ramirez",
+    motivo: "Bajo rendimiento académico"
+  }
+];
+
+mostrarCitas();
+
+function mostrarCitas() {
+  listaCitas.innerHTML = "";
+
+  if (citas.length === 0) {
+    mensajeVacio.style.display = "block";
+    return;
+  }
+
+  mensajeVacio.style.display = "none";
+
+  citas.forEach(cita => {
+    listaCitas.innerHTML += `
+      <div class="row fila-cita text-center">
+        <div class="col-md-4">
+          <p>${cita.fechaHora}</p>
+        </div>
+
+        <div class="col-md-4">
+          <p>${cita.estudiante}</p>
+        </div>
+
+        <div class="col-md-4 motivo-cita">
+          <p>${cita.motivo}</p>
+        </div>
+      </div>
+    `;
+  });
+}*/
